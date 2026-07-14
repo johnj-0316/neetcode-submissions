0f375101319dc26b042e5493022b682d6f14dffe
John Martin <jjoseph0575@gmail.com>
2026-07-14 13:10:42 -0400

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.js
@@ -0,0 +1,52 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        let i = 0;
+        let j = matrix.length;
+        
+        while (i <= j) {
+            let m = Math.floor(i + (j - i) / 2);
+
+            if (!matrix[m]) {
+                return false;
+            }
+
+            let leftmost = matrix[m][0];
+            let rightmost = matrix[m][matrix[m].length - 1];
+
+            if (leftmost <= target && rightmost >= target) {
+                let left = 0;
+                let right = matrix[m].length;
+
+                while (left <= right) {
+                    let mid = Math.floor(left + (right - left) / 2);
+
+                    if (matrix[m][mid] === target) {
+                        return true;
+                    }
+                    else if (matrix[m][mid] > target) {
+                        right = mid - 1;
+                    }
+                    else {
+                        left = mid + 1;
+                    }
+                }
+
+                return false;
+            }
+            else if (leftmost >= target) {
+                j = m - 1;
+            }
+            else {
+                i = m + 1;
+                console.log(i, j)
+            }
+        }
+
+        return false;
+    }
+}
