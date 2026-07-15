17a14077fb3fde51d8d51e0f0b1df13dcd1d3296
John Martin <jjoseph0575@gmail.com>
2026-07-14 21:42:31 -0400

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        if (prices.length === 1) {
+            return 0;
+        }
+
+        let left = 0;
+        let max = 0;
+
+        for (let right = 1; right < prices.length; right++) {
+            let leftPrice = prices[left];
+            let rightPrice = prices[right];
+
+            if (rightPrice < leftPrice) {
+                left = right;
+            }
+            else {
+                max = Math.max(rightPrice - leftPrice, max);
+            }
+        }
+
+        return max;
+    }
+}
